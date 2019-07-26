4971989a795b5a0325faaca7479cdac1c14dddee
manisankar27 <53298567+manisankar27@users.noreply.github.com>
2019-07-26 13:55:03 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if((n=='a'||n=='e'||n=='i'||n=='o'||n=='u')||(n=='A'||n=='E'||n=='I'||n=='O'||n=='U'))
+    {
+    printf("vowels");
+    }
+    else
+    {
+    printf("consonant");
+    }
+    return 0;
+}
