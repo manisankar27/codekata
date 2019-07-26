1473fbedd6f248c478a836ac5b497d1e6d612fca
manisankar27 <53298567+manisankar27@users.noreply.github.com>
2019-07-26 14:58:38 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    //char n;
+    //scanf("%d",&a);
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("HELLO \n");
+    }
+    return 0;
+}
